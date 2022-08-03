a3e847974f25c8f650b3edb3d6e67a027b270ca0
satrn8 <verypyc@gmail.com>
2022-08-03 22:19:04 +0300

    commit 08/03

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -4,11 +4,16 @@ from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
 
+
 def test_github():
-    browser.open_url("https://github.com")
+    browser.open("https://github.com")
     s(".header-search-input").click()
     s(".header-search-input").send_keys("eroshenkoam/allure-example")
     s(".header-search-input").submit()
     s(by.link_text("eroshenkoam/allure-example")).click()
     s("#issues-tab").click()
     s(by.partial_text("#76")).should(be.visible)
+
+
+#  allure serve tests\allure-results
+
